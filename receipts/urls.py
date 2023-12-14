97aea03781c1c8184bacca4944ba9303c767b8dc
kevitruon <kevitruon@gmail.com>
2023-12-14 11:52:51 -0800

    Feature 6 Complete

--- receipts/urls.py
+++ receipts/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from receipts.views import receipt_list
 
-app_name = 'receipts'
-
 urlpatterns = [
-    path('', receipt_list, name='receipt_list'),
-
+    path("", receipt_list, name="home"),
 ]
--- receipts/views.py
+++ receipts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from receipts.models import Receipt
 
 def receipt_list(request):
